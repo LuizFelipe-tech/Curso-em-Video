287557fb180ac8ab20b870d732d59eeb280c75ca
LuizFelipe-tech <luizfelipemelos@gmail.com>
2024-10-10 12:53:53 -0300

    c28

--- Projetos/portifolio/script.js
+++ Projetos/portifolio/script.js
@@ -1,48 +1,56 @@
 var divHtml = document.querySelector('#html');
 var imgHtml = document.querySelector('#imghtml');
-var control = 1;
-var closeButton = document.createElement('img');
-var paragraph = document.createElement('p')
-closeButton.setAttribute('src', 'image/fechar.png')
-divHtml.addEventListener('click',() => clicarHTML(divHtml, imgHtml));
-function clicarHTML(x, y) {
-    if (control == true) {
-        setTimeout(function() {
-            x.appendChild(paragraph);
-            paragraph.style.position = 'relative';
-            paragraph.style.top = '-20%';
-            console.log('Inserção parágrafo')
-            paragraph.innerHTML = 'Tenho amplo domínio de HTML, o que me permite criar e estruturar páginas web de maneira eficiente, organizando textos, imagens e outros elementos de mídia. Sou capaz de desenvolver layouts responsivos que se adaptam a diferentes dispositivos, criar formulários interativos para coleta de dados, otimizar o conteúdo para SEO com tags adequadas, e integrar o HTML com CSS e JavaScript para estilizar e adicionar funcionalidades dinâmicas às páginas. Isso me permite desenvolver sites funcionais, acessíveis e prontos para interações com os usuários.';
-        }, 1500);
-        x.style.transitionDuration = '2s';
-        x.style.width = '500px';
-        x.style.height = '250px';
-        x.style.position = 'absolute';
-        x.style.top = '50%';
-        x.style.left = '50%';
-        x.style.transform = 'translateX(-50%)';
-        y.style.display = 'block';
-        y.style.margin = 'auto';
-        x.appendChild(closeButton);
-        closeButton.style.width = '40px';
-        closeButton.style.width = '40px';
-        closeButton.style.position = 'relative';
-        closeButton.style.left = '90%';
-        closeButton.style.bottom = '40%';
-        closeButton.addEventListener('click', fechar);
-        console.log(control)
-        control = false
-        console.log(control)                      
+var paragraphHtml = document.querySelector('#paragraphHtml')
+var divCss = document.querySelector('#css')
+var imgCss = document.querySelector('cssimg')
+var paragraphCss = document.querySelector('paragraphCss')
+var closeButton = document.querySelector('#closeButton');
+var isHtmlContracted = true;
+
+divHtml.classList.add('contractedHtml')
+imgHtml.classList.add('imgHtmlCOntracted')
+closeButton.classList.add('HiddenCloseButton')
+paragraphHtml.classList.add('hiddenParagraph')
+
+divHtml.addEventListener('click',() => clickHappened(divHtml, imgHtml, paragraphHtml));
+divCss.addEventListener('click', ()=> clickHappened(divCss, imgCss, paragraphCss))
+
+function clickHappened(element, imgElement, paragraphElement) {
+    function fechar(click){
+        click.stopPropagation()
+        closeButton.classList.remove('ShowedCloseButton')
+        closeButton.classList.add('HiddenCloseButton')
+        element.classList.remove('expanded')
+        element.classList.add('expandedToContractedHtml')
+        setTimeout(function(){
+            element.classList.remove('expandedToContractedHtml')
+            element.classList.add('contractedHtml')
+        }, 2001)
+        imgElement.classList.add('imgExpandedToContracted')
+        imgElement.classList.remove('HtmlExpanded')
+        paragraphElement.classList.remove('showParagraph')
+        paragraphElement.classList.add('hiddenParagraph')
+        isHtmlContracted = true
     }
-}
-function fechar(click){
-    click.stopPropagation()
-    divHtml.removeChild(paragraph)
-    divHtml.style.width = '100px'
-    divHtml.style.height = '100px'
-    divHtml.style.top = '95%'
-    divHtml.style.left = '10%'
-    imgHtml.style.display = ''
-    divHtml.removeChild(closeButton)
-    control = true
+
+    if (isHtmlContracted == true) {
+
+        setTimeout(function() {
+            paragraphElement.classList.remove('hiddenParagraph')
+            paragraphElement.classList.add('showParagraph')
+            closeButton.classList.remove('HiddenCloseButton')
+            closeButton.classList.add('ShowedCloseButton')
+            closeButton.addEventListener('click', fechar)
+            switch(element){
+                case divHtml:
+                    paragraphElement.innerHTML = 'Tenho amplo domínio de HTML, o que me permite criar e estruturar páginas web de maneira eficiente, organizando textos, imagens e outros elementos de mídia. Sou capaz de desenvolver layouts responsivos que se adaptam a diferentes dispositivos, criar formulários interativos para coleta de dados, otimizar o conteúdo para SEO com tags adequadas, e integrar o HTML com CSS e JavaScript para estilizar e adicionar funcionalidades dinâmicas às páginas. Isso me permite desenvolver sites funcionais, acessíveis e prontos para interações com os usuários.';
+            }
+        }, 1500);
+
+        element.classList.remove('contractedHtml')
+        element.classList.add('expanded')
+        imgElement.classList.remove('contractedHtml')
+        imgElement.classList.add('imgHtmlExpanded')
+        isHtmlContracted = false                      
+    }
 }
