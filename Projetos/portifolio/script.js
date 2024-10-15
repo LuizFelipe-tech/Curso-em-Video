11de7ea97a6d7adecc33bb36c7f9ab64f26acdd6
LuizFelipe-tech <81056246+LuizFelipe-tech@users.noreply.github.com>
2024-10-15 17:26:12 -0300

    Update script.js

--- Projetos/portifolio/script.js
+++ Projetos/portifolio/script.js
@@ -28,58 +28,81 @@ paragraphCss.classList.add('hiddenParagraph')
 paragraphJavascript.classList.add('hiddenParagraph')
 imgHtml.classList.add('imgContracted')
 greyBackground.classList.add('greyBackgroundOff')
-
-    
+  
 divHtml.addEventListener('click',() => clickHappened(divHtml, closeButtonHtml, paragraphHtml));
 divCss.addEventListener('click', ()=> clickHappened(divCss, closeButtonCss, paragraphCss))
 divJavascript.addEventListener('click', ()=> clickHappened(divJavascript, closeButtonJavascript, paragraphJavascript))
 
 function clickHappened(element, closeButton, paragraphElement) {
-console.log(isWhichElement)
-if(!isWhichElement == undefined || !isElementContracted == true) return;
-    elementClicked()
-    function elementClicked(){
-        switch (element){
-            case divHtml: 
-                isWhichElement = ['Html', element]
-                break
-            case divCss:
-                isWhichElement = ['Css', element]
-        }
-    }
-    function fechar(click){
-        click.stopPropagation()
-        closeButton.classList.remove('ShowedCloseButton')
-        closeButton.classList.add('HiddenCloseButton')
-        element.classList.remove('expanded')
-        element.classList.add(`expandedToContracted${isWhichElement[0]}`)
-        if(!isWhichElement[1] == element) return;
-        setTimeout(function(){
-            if(!isWhichElement[1] == element) return;
-            console.log(isWhichElement[0])
-            element.classList.remove(`expandedToContracted${isWhichElement[0]}`)
-            element.classList.add(`contracted${isWhichElement[0]}`)
-        }, 2001)
-        paragraphElement.classList.remove('showParagraph')
-        paragraphElement.classList.add('hiddenParagraph')
-        greyBackground.classList.remove('greyBackgroundOn')
-        greyBackground.classList.add('greyBackgroundOff')
-        isElementContracted = true
-    }
-
-    if (isElementContracted == true) {
-
-        setTimeout(function() {
-            paragraphElement.classList.remove('hiddenParagraph')
-            paragraphElement.classList.add('showParagraph')
-            closeButton.classList.remove('HiddenCloseButton')
-            closeButton.classList.add('ShowedCloseButton')
+    //if(!isWhichElement == undefined || !isElementContracted == true) return;
+    switch (element){
+        case divHtml:
+            console.log('Executado 1')
             closeButton.addEventListener('click', fechar)
-        }, 1500);
-        greyBackground.classList.remove('greyBackgroundOff')
-        greyBackground.classList.add('greyBackgroundOn')
-        element.classList.remove(`contracted${isWhichElement[0]}`)
-        element.classList.add('expanded')
-        isElementContracted = false                      
+            function fechar(click){
+                click.stopPropagation()
+                closeButton.classList.remove('ShowedCloseButton')
+                closeButton.classList.add('HiddenCloseButton')
+                element.classList.remove('expanded')
+                element.classList.add(`expandedToContractedHtml`)
+                setTimeout(function(){
+                    element.classList.remove(`expandedToContractedHtml`)
+                    element.classList.add(`contractedHtml`)
+            }, 2001)
+            paragraphElement.classList.remove('showParagraph')
+            paragraphElement.classList.add('hiddenParagraph')
+            greyBackground.classList.remove('greyBackgroundOn')
+            greyBackground.classList.add('greyBackgroundOff')
+            isElementContracted = true
+            }
+            if (isElementContracted == true) {
+
+                setTimeout(function() {
+                    paragraphElement.classList.remove('hiddenParagraph')
+                    paragraphElement.classList.add('showParagraph')
+                    closeButton.classList.remove('HiddenCloseButton')
+                    closeButton.classList.add('ShowedCloseButton')
+                }, 1500);
+                greyBackground.classList.remove('greyBackgroundOff')
+                greyBackground.classList.add('greyBackgroundOn')
+                element.classList.remove(`contractedHtml`)
+                element.classList.add('expanded')
+                isElementContracted = false                      
+            }
+        break;
+    case divCss:
+        console.log('Executado 2')
+        closeButton.addEventListener('click', fechar)
+        function fechar(click){
+            click.stopPropagation()
+            closeButton.classList.remove('ShowedCloseButton')
+            closeButton.classList.add('HiddenCloseButton')
+            element.classList.remove('expanded')
+            element.classList.add(`expandedToContractedHCss`)
+            setTimeout(function(){
+                element.classList.remove(`expandedToContractedCss`)
+                element.classList.add(`contractedCss`)
+            }, 2001)
+            paragraphElement.classList.remove('showParagraph')
+            paragraphElement.classList.add('hiddenParagraph')
+            greyBackground.classList.remove('greyBackgroundOn')
+            greyBackground.classList.add('greyBackgroundOff')
+            isElementContracted = true
+        }
+        if (isElementContracted == true) {
+
+            setTimeout(function() {
+                paragraphElement.classList.remove('hiddenParagraph')
+                paragraphElement.classList.add('showParagraph')
+                closeButton.classList.remove('HiddenCloseButton')
+                closeButton.classList.add('ShowedCloseButton')
+            }, 1500);
+            greyBackground.classList.remove('greyBackgroundOff')
+            greyBackground.classList.add('greyBackgroundOn')
+            element.classList.remove(`contractedCss`)
+            element.classList.add('expanded')
+            isElementContracted = false                      
+        }
+    break
     }
 }
